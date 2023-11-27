085da8e683d35d1b946d91e61fda8dc3e960eef9
Nextly <nextly@air-de-yuri.lan>
2023-11-27 11:03:17 -0300

    links for designer

---
+++ assets/js/darkMode.js
@@ -0,0 +1,15 @@
+const closeAd = () => {
+    const ad = document.querySelector('.advertising-banner');
+    ad.style.display = 'none';
+}
+function toggleMode() {
+    const stylesheet = document.getElementById('stylesheet');
+
+    if (stylesheet.getAttribute('href') === 'assets/css/styles.css') {
+        stylesheet.setAttribute('href', 'assets/css/styles-dark.css');
+    } else {
+        stylesheet.setAttribute('href', 'assets/css/styles.css');
+    }
+}
+document.getElementById('mode-toggle').addEventListener('click', toggleMode);
+document.getElementById('mode-toggle-mobile').addEventListener('click', toggleMode);
