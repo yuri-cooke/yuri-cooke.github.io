83950eff69606585f353a81880dc6a7e51b2c333
Nextly <nextly@air-de-yuri.lan>
2023-12-07 12:23:10 -0300

    rankingpageDesk1st

--- bet-sports-usa/js/darkMode.js
+++ bet-sports-usa/js/darkMode.js
@@ -2,10 +2,10 @@
 function toggleMode() {
     const stylesheet = document.getElementById('stylesheet');
 
-    if (stylesheet.getAttribute('href') === 'assets/css/light-mode.css') {
-        stylesheet.setAttribute('href', 'assets/css/dark-mode.css');
+    if (stylesheet.getAttribute('href') === 'css/light-mode.css') {
+        stylesheet.setAttribute('href', 'css/dark-mode.css');
     } else {
-        stylesheet.setAttribute('href', 'assets/css/light-mode.css');
+        stylesheet.setAttribute('href', 'css/light-mode.css');
     }
 
     console.log(stylesheet)
